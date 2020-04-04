550b122e3d20d234acad2e0582f8bd81d10e9b81
unknown <danielsimonchin@hotmail.com>
2020-04-04 19:48:47 -0400

    finishing up

--- Puzzle/js/puzzle.js
+++ Puzzle/js/puzzle.js
@@ -1,114 +1,190 @@
-var listPlayerArr = []; //list of all players
-var moveCount; // amount of moves it took the player at end of game
-var gameWon; //if the currentplayer has won the game
+//////////// GLOBAL VARIABLES
+var chrono; //chrono for stopwatch
+var utility;
+var playerManager;
 var puzzleGameObj;
-//constructor
-function Player(moves, status) {
-  var name = document.getElementById("nameInput");
-  var boardDimension = document.getElementById('dimensionList').options[document.getElementById("dimensionList").selectedIndex].value;
-  var numMoves = moves;
-  var duration = document.getElementById('displaySeconds');
-  var theStatus = status;
+var gameWon;
+var wrongMove;
+////////////  CONSTRUCTORS
+function Player(name, boardDimension, moves, duration, status) {
+  this.name = name;
+  this.boardDimension = boardDimension;
+  this.numMoves = moves;
+  this.duration = duration;
+  this.theStatus = status;
+}
+
+function PlayerManager() {
+  this.listPlayerArr = [];
+  this.gameCounter = 0;
+  this.gameDuration = 0;
+  this.nberMove = 0;
+  this.storeGameStats = storeGameStats;
+
+  function storeGameStats(theStatus) {
+    //instantiate player
+    var name = document.getElementById("nameInput").value;
+    var dim = document.getElementById("dimensionList").options[document.getElementById("dimensionList").selectedIndex].value;
+    var moves = document.getElementById("displayMoves").value;
+    var duration = document.getElementById("displaySeconds").value;
+    var player = new Player(name, dim, moves, duration, theStatus);
+    //add player
+    this.listPlayerArr.push(player);
+
+    //reset stats
+    this.gameCounter++;
+    this.gameDuration = 0;
+    this.nberMoves = 0;
+  }
+}
+/////////////// UTILITY FUNCTIONS
+function Utility() {
+  this.generateRandomNumber = generateRandomNumber;
+  this.sampleEasyBoardTest = sampleEasyBoardTest;
+  this.terminateGame = terminateGame;
+  this.clearDisplay = clearDisplay;
+  this.cancelPuzzlePlay = cancelPuzzlePlay;
+  this.checkFormFilled = checkFormFilled;
+  this.enableButton = enableButton;
+  this.showChrono = showChrono;
+  this.showStats = showStats;
+  this.playAudio = playAudio;
+
+  function generateRandomNumber(minValue, maxValue) {
+    return (Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue);
+  }
+
+
+
+  function playAudio() { //not completed
+    if (gameWon == true) {
+      var audio = new Audio('sounds/Firework.mp3');
+      audio.play();
+      gameWon = false;
+    }
+    if (wrongMove == true) {
+      var audio = new Audio('sounds/beep-07.mp3');
+      audio.play();
+      wrongMove = false;
+    }
+  }
+  //used for testing (2 moves away from winning)
+  function sampleEasyBoardTest() {
+    var easyBoard = [];
+    var arr1 = [new Tile(0, 0, 1), new Tile(0, 1, 2), new Tile(0, 2, 3)];
+    var arr2 = [new Tile(1, 0, 4), new Tile(1, 1, 0), new Tile(1, 2, 6)];
+    var arr3 = [new Tile(2, 0, 7), new Tile(2, 1, 5), new Tile(2, 2, 8)];
+    easyBoard = [arr1, arr2, arr3];
+    return easyBoard;
+  }
+
+  function terminateGame(theStatus) {
+    //TODO
+    playerManager.storeGameStats(theStatus);
+    clearInterval(chrono);
+    //document.getElementById('dimensionList').options[document.getElementById('dimensionList').selectedIndex].value = 0;
+    clearDisplay();
+    showStats();
+  }
+
+  function clearDisplay() {
+    chrono = undefined;
+    document.getElementById('displayMoves').value = "";
+    document.getElementById('displayHours').value = "";
+    document.getElementById('displaySeconds').value = "";
+    document.getElementById('displayMinutes').value = "";
+  }
+
+  function cancelPuzzlePlay() {
+    if (gameWon == true) {
+      terminateGame("success");
+    } else {
+      terminateGame("cancelled");
+    }
+    enableButton("redButton", true);
+    enableButton("greenButton", false);
+  }
+
+  function checkFormFilled() {
+    var dimSize = document.getElementById('dimensionList');
+
+    if (document.getElementById("nameInput").value == "" || dimSize.options[dimSize.selectedIndex].value == 0 || chrono != undefined) {
+      enableButton("greenButton", true);
+    } else {
+      enableButton("greenButton", false);
+    }
+  }
+
+  function enableButton(btnId, theStatus) {
+    document.getElementById(btnId).disabled = theStatus;
+  }
+
+  function showChrono() {
+    //document.getElementById("displayMoves").value = 0;
+    var sec = document.getElementById("displaySeconds");
+    var min = document.getElementById("displayMinutes");
+    var hrs = document.getElementById("displayHours");
+    sec.value = 0;
+    min.value = 0;
+    hrs.value = 0;
+    var totalTime = 0;
+    chrono = setInterval(function() {
+      sec.value = totalTime++;
+      if (sec.value == 60) {
+        min.value++;
+        totalTime = 0;
+        sec.value = 0;
+        if (min.value == 60) {
+          hrs.value++;
+          min.value = 0;
+        }
+      }
+    }, 1000);
+  }
+
+  function showStats() {
+    var tableBegin = "<table>";
+    var heading = "<th>#</th><th>Name</th><th>Dimension</th><th>Moves</th><th>Duration</th><th>Status</th>";
+    var content = "";
+    for (var i = 1; i < playerManager.listPlayerArr.length + 1; i++) {
+      content = content + "<tr>" +
+        "<td>" + i + "</td>" +
+        "<td>" + playerManager.listPlayerArr[i - 1].name + "</td>" +
+        "<td>" + playerManager.listPlayerArr[i - 1].boardDimension + "</td>" +
+        "<td>" + playerManager.listPlayerArr[i - 1].numMoves + "</td>" +
+        "<td>" + playerManager.listPlayerArr[i - 1].duration + "</td>" +
+        "<td>" + playerManager.listPlayerArr[i - 1].theStatus + "</td>" +
+        "</tr>";
+    }
+
+    var tableEnd = "</table>";
+    document.getElementById("leaderboardsTable").innerHTML = tableBegin + heading + content + tableEnd;
+  }
 }
 
 function playGame() {
-  document.getElementById("redButton").disabled = false;
-  createPuzzle();
-  chronoStart();
+  document.getElementById('displayMoves').value = 0;
+  utility.enableButton("redButton", false);
+  utility.enableButton("greenButton", true);
+  utility.enableButton("nameInput", true);
+  utility.enableButton("dimensionList", true);
+  puzzleGameObj = new PuzzleGame();
+  drawPuzzleBoard();
+  document.querySelector('.puzzleBoard').style.display = 'block';
+  setupTileClickEvent();
+  utility.showChrono();
 }
 
 function cancelGame() {
-  document.getElementById("redButton").disabled = true;
-  var player = new Player(moveCount, "cancelled")
+  utility.cancelPuzzlePlay("cancelled");
+  utility.enableButton("nameInput", false);
+  utility.enableButton("dimensionList", false);
+  document.querySelector('.puzzleBoard').style.display = 'none';
+  puzzleGameObj = new PuzzleGame();
 }
 
-function wonGame() {
 
-}
-
-/*function createPuzzle() {
-
-  var tableBegin = '<table border="thick">'
-
-  var content;
-  var dropDownMenu = document.getElementById("dimensionList");
-  if (dropDownMenu.options[dropDownMenu.selectedIndex].value == 3) {
-    for (var i = 0; i < 3; i++) {
-      content = content + "<tr>" + "<td>" + i + 1 + "</td>" +
-        "<td>" + i + 2 + "</td>" +
-        "<td>" + i + 3 + "</td>" + "</tr>";
-
-    }
-  } else if (dropDownMenu.options[dropDownMenu.selectedIndex].value == 4) {
-    for (var i = 0; i < 4; i++) {
-      content = content + "<tr>" + "<td>" + i + 1 + "</td>" +
-        "<td>" + i + 2 + "</td>" +
-        "<td>" + i + 3 + "</td>" + "</tr>";
-
-    }
-  } else if (dropDownMenu.options[dropDownMenu.selectedIndex].value == 5) {
-    for (var i = 0; i < 5; i++) {
-      content = content + "<tr>" + "<td>" + i + 1 + "</td>" +
-        "<td>" + i + 2 + "</td>" +
-        "<td>" + i + 3 + "</td>" + "</tr>";
-
-    }
-  }
-  var tableEnding = "</table>"
-  document.getElementbyId('gameTable').innerHtml = tableBegin + tr_heading + content + tableEnding;
-}*/
-
-///////////////////// all Helper methods
-
-function generateRandomNumber(minValue, maxValue) {
-  //taken from w3schools at https://www.w3schools.com/js/js_random.asp
-  return Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
-}
-
-function playAudio() { //not completed
-  if (gameWon == true) {
-    var audio = new Audio('sounds/Firework.mp3');
-    audio.play();
-  } else if (gameWon == false) {
-    var audio = new Audio('sounds/beep-07.mp3');
-    audio.play();
-  }
-}
-
-function sampleEasyBoardTest() {
-  //TODO
-}
-
-function terminateGame(theStatus) {
-  //TODO
-  clearInterval(); //TODO
-  setGameInfo_Stats(theStatus);
-  document.getElementById("nameInput").value = "";
-  moveCount = 0;
-  document.getElementById('dimensionList').options[document.getElementById('dimensionList').selectedIndex].value = 3;
-  document.getElementById('displayMoves').value = 0;
-  document.getElementById('displayHours').value = 0;
-  document.getElementById('displaySeconds').value = 0;
-  document.getElementById('displayMinutes').value = 0;
-}
-
-function cancelPuzzlePlay() {
-  terminateGameInfo("cancelled");
-  document.getElementById("redButton").disabled = true;
-}
-
-function checkFormFilled() {
-  if (document.getElementById("nameInput").value == "") {
-    document.getElementById("greenButton").disabled = true;
-  } else {
-    document.getElementById("greenButton").disabled = false;
-  }
-}
-
-function enableButton(btnId, theStatus, btnClass) {
-
-}
-
+//GAME LOGIC
 //5.3 Tile
 function Tile(row, col, indexNumber) {
   this.row = row;
@@ -133,7 +209,8 @@ function checkTileType(num) {
 function PuzzleGame() {
   //calls its function members to set the fields
   this.puzzleWidth = document.getElementById('dimensionList').value;
-  this.puzzleBoard = createBoardStructure();
+  //this.puzzleBoard = createBoardStructure();
+  this.puzzleBoard = utility.sampleEasyBoardTest();
   this.goalState = createGoalState();
 }
 
@@ -184,7 +261,7 @@ function createBoardStructure() {
     puzzleArr[i] = [];
     for (var j = 0; j < puzzleWidth; j++) {
       //the number from indexes that will be placed in the array
-      var positionRemove = generateRandomNumber(0, indexes.length - 1);
+      var positionRemove = utility.generateRandomNumber(0, indexes.length - 1);
       //remove the value at that index and store it into the variable
       var removedValue = indexes.splice(positionRemove, 1);
       //creating the Tile object and placing it
@@ -195,17 +272,28 @@ function createBoardStructure() {
 }
 
 function drawPuzzleBoard() {
-  var puzzleBoardDiv = document.createElement('div');
-  puzzleBoardDiv.class = "puzzleBoard";
+  //remove the current board and create a new one with a new puzzleBoard layout if it doesn't exist
+  console.log("hi");
+  var currentBoard = document.querySelector('.puzzleBoard');
+  if (currentBoard != null) {
+    currentBoard.remove();
+  }
 
-  var htmlTable = "<table>";
+  var puzzleBoardDiv = document.createElement('div');
+  puzzleBoardDiv.className = "puzzleBoard";
+
+  var htmlTable = "<table id='puzzleTable'>";
   var htmlRows = "";
 
   for (var i = 0; i < puzzleGameObj.puzzleWidth; i++) {
     htmlRows += "<tr>";
     for (var j = 0; j < puzzleGameObj.puzzleWidth; j++) {
       //appending all the array elements
-      htmlRows += "<td>" + "<button>" + puzzleGameObj.puzzleBoard[i][j].indexNumber + "</button>" + "</td>";
+      if (puzzleGameObj.puzzleBoard[i][j].indexNumber == 0) {
+        htmlRows += "<td class='emptyTile'></td>";
+      } else {
+        htmlRows += "<td class='filledType'>" + puzzleGameObj.puzzleBoard[i][j].indexNumber + "</button>" + "</td>";
+      }
     }
     htmlRows += "</tr>";
   }
@@ -215,35 +303,35 @@ function drawPuzzleBoard() {
 }
 //helper that takes as input the value that is on the table
 //works,tested in console
-function findTile(indexNumber)
-{
+function findTile(indexNumber) {
   var coordinate = [];
-  for(var i = 0 ; i < puzzleGameObj.puzzleBoard.length;i++)
-  {
-    for(var j = 0 ; j < puzzleGameObj.puzzleBoard[i].length;j++)
-    {
-      if(puzzleGameObj.puzzleBoard[i][j].indexNumber == indexNumber)
-      {
-        coordinate = [i,j];
+  for (var i = 0; i < puzzleGameObj.puzzleBoard.length; i++) {
+    for (var j = 0; j < puzzleGameObj.puzzleBoard[i].length; j++) {
+      if (puzzleGameObj.puzzleBoard[i][j].indexNumber == indexNumber) {
+        coordinate = [i, j];
         return coordinate;
       }
     }
   }
 }
+//first contains the position of the current, second param contains position of the place to move to.
+function swap2Tiles(indexTile1, indexTile2) {
+  var tileCurrent = puzzleGameObj.puzzleBoard[indexTile1[0]][indexTile1[1]];
+  var neighbour = puzzleGameObj.puzzleBoard[indexTile2[0]][indexTile2[1]];
 
-function swap2Tiles(indexTile1,indexTile2)
-{
+  var tempIndexNumber = puzzleGameObj.puzzleBoard[indexTile1[0]][indexTile1[1]].indexNumber;
 
+  puzzleGameObj.puzzleBoard[indexTile1[0]][indexTile1[1]].indexNumber = neighbour.indexNumber;
+  puzzleGameObj.puzzleBoard[indexTile1[0]][indexTile1[1]].tileType = 'emptyTile';
+
+  puzzleGameObj.puzzleBoard[indexTile2[0]][indexTile2[1]].indexNumber = tempIndexNumber;
+  puzzleGameObj.puzzleBoard[indexTile2[0]][indexTile2[1]].tileType = 'filledType';
 }
 //works, tested in console
-function match2States(state1,state2)
-{
-  for(var i = 0; i < state1.length; i++)
-  {
-    for(var j = 0 ; j < state1[i].length; j++)
-    {
-      if(state1[i][j] != state2[i][j])
-      {
+function match2States(state1, state2) {
+  for (var i = 0; i < state1.length; i++) {
+    for (var j = 0; j < state1[i].length; j++) {
+      if (state1[i][j].indexNumber != state2[i][j]) {
         return false;
       }
     }
@@ -251,65 +339,93 @@ function match2States(state1,state2)
   return true;
 }
 //returns indexNumber properties of the tiles surounding the tile at "position" ex:[0,2]
-function getNeighboursIndicesArr(position)
-{
+function getNeighboursIndicesArr(position) {
   var returnValues = [];
 
-  var north = [position[0]-1,position[1]];
-  if(north[0] < 0 || north[1] < 0 || north[0] >= puzzleGameObj.puzzleWidth || north[1] >= puzzleGameObj.puzzleWidth)
-  {
+  var north = [position[0] - 1, position[1]];
+  if (north[0] < 0 || north[1] < 0 || north[0] >= puzzleGameObj.puzzleWidth || north[1] >= puzzleGameObj.puzzleWidth) {
     returnValues[0] = -1;
+  } else {
+    returnValues[0] = puzzleGameObj.puzzleBoard[north[0]][north[1]].indexNumber;
   }
-  else
-  {
-    returnValues[0] = puzzleGameObj[north[0]][north[1]].indexNumber;
-  }
-  var east = [position[0],position[1]+1];
-  if(east[0] < 0 || east[1] < 0 || east[0] >= puzzleGameObj.puzzleWidth || east[1] >= puzzleGameObj.puzzleWidth)
-  {
+  var east = [position[0], position[1] + 1];
+  if (east[0] < 0 || east[1] < 0 || east[0] >= puzzleGameObj.puzzleWidth || east[1] >= puzzleGameObj.puzzleWidth) {
     returnValues[1] = -1;
+  } else {
+    returnValues[1] = puzzleGameObj.puzzleBoard[east[0]][east[1]].indexNumber;
   }
-  else
-  {
-    returnValues[1] = puzzleGameObj[east[0]][east[1]].indexNumber;
-  }
-  var south = [position[0]+1,position[1]];
-  if(south[0] < 0 || south[1] < 0 || south[0] >= puzzleGameObj.puzzleWidth || south[1] >= puzzleGameObj.puzzleWidth)
-  {
+  var south = [position[0] + 1, position[1]];
+  if (south[0] < 0 || south[1] < 0 || south[0] >= puzzleGameObj.puzzleWidth || south[1] >= puzzleGameObj.puzzleWidth) {
     returnValues[2] = -1;
+  } else {
+    returnValues[2] = puzzleGameObj.puzzleBoard[south[0]][south[1]].indexNumber;
   }
-  else
-  {
-    returnValues[2] = puzzleGameObj[south[0]][south[1]].indexNumber;
-  }
-  var west = [position[0],position[1]-1];
-  if(west[0] < 0 || west[1] < 0 || west[0] >= puzzleGameObj.puzzleWidth || west[1] >= puzzleGameObj.puzzleWidth)
-  {
+  var west = [position[0], position[1] - 1];
+  if (west[0] < 0 || west[1] < 0 || west[0] >= puzzleGameObj.puzzleWidth || west[1] >= puzzleGameObj.puzzleWidth) {
     returnValues[3] = -1;
-  }
-  else
-  {
-    returnValues[3] = puzzleGameObj[west[0]][west[1]].indexNumber;
+  } else {
+    returnValues[3] = puzzleGameObj.puzzleBoard[west[0]][west[1]].indexNumber;
   }
   return returnValues;
+
 }
-function processClickTile(indexValue)
-{
+
+function processClickTile(indexValue) {
   //finds position of tile
-  var position = findTile(indexValue);
+  var current = findTile(indexValue);
+  //calls getNeighboursIndicesArr so we know the indexNumbers of the getNeighbours
+  var neighbours = getNeighboursIndicesArr(current);
 
+  //the neighbour that has the 0 value
+  var indexMoveTo;
+  for (var i = 0; i < neighbours.length; i++) {
+    if (neighbours[i] == 0) {
+      indexMoveTo = neighbours[i];
+    }
+  }
 
+  if (indexMoveTo != null) {
+    var a = document.getElementById('displayMoves');
+    a.value++;
+    var moveTo = findTile(indexMoveTo);
+    swap2Tiles(current, moveTo);
+    //redraw updated board
+    drawPuzzleBoard();
+    setupTileClickEvent();
+    if (match2States(puzzleGameObj.puzzleBoard, puzzleGameObj.goalState) == true) {
+      gameWon = true;
+      cancelGame();
+      utility.playAudio();
+      puzzleGameObj = new PuzzleGame();
+    }
+  } else {
+    wrongMove = true;
+    utility.playAudio();
+  }
 }
 
+function callProcessClickTile(e) {
+  processClickTile(e.target.innerHTML);
+}
+//making it so all the table buttons are clickable and call the processClickTile method to move tiles
+function setupTileClickEvent() {
+  var tableChildren = document.querySelectorAll('.filledType');
+  for (var i = 0; i < tableChildren.length; i++) {
+    tableChildren[i].addEventListener('click', callProcessClickTile);
+  }
+}
 
-
-function process() {
-  //document.getElementById("nameInput").addEventListener("input", checkFormFilled);
-  //document.getElementById("greenButton").addEventListener("click", playGame);
-  //document.getElementById("redButton").addEventListener("click", cancelGame);
+function init() {
+  gameWon = false;
+  wrongMove = false;
+  utility = new Utility();
+  playerManager = new PlayerManager();
   puzzleGameObj = new PuzzleGame();
-  drawPuzzleBoard();
+  document.getElementById("nameInput").addEventListener("input", utility.checkFormFilled);
+  document.getElementById("dimensionList").addEventListener("input", utility.checkFormFilled);
 
+  document.getElementById("greenButton").addEventListener("click", playGame);
+  document.getElementById("redButton").addEventListener("click", cancelGame);
 }
 
-document.addEventListener('DOMContentLoaded', process);
+document.addEventListener('DOMContentLoaded', init);
